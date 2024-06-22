9816880b972f3c47d9ad71bf41beaa714b3b4e80
Your Name <brendangerardcasey@gmail.com>
2024-06-22 08:12:32 -0600

    commented functions

--- landsat_indices_and_masks.js
+++ landsat_indices_and_masks.js
@@ -1,6 +1,11 @@
 // Define Landsat indices functions
 
-// Add Bare Soil Index (BSI) band
+/**
+ * Adds Bare Soil Index (BSI) band to an image.
+ * BSI = ((Red + SWIR) - (NIR + Blue)) / ((Red + SWIR) + (NIR + Blue))
+ * @param {Object} image - The image to process.
+ * @returns {Object} The image with the BSI band added.
+ */
 exports.addBSI = function(image) {
   var BSI = image.expression(
     '((Red + SWIR) - (NIR + Blue)) / ((Red + SWIR) + (NIR + Blue))', {
@@ -12,7 +17,12 @@ exports.addBSI = function(image) {
   return image.addBands([BSI]);
 };
 
-// Add Disease Stress Water Index (DSWI) band
+/**
+ * Adds Disease Stress Water Index (DSWI) band to an image.
+ * DSWI = (NIR + Green) / (Red + SWIR)
+ * @param {Object} image - The image to process.
+ * @returns {Object} The image with the DSWI band added.
+ */
 exports.addDSWI = function(image) {
   var DSWI = image.expression(
     '(NIR + Green) / (Red + SWIR)', {
@@ -24,7 +34,12 @@ exports.addDSWI = function(image) {
   return image.addBands([DSWI]);
 };
 
-// Add Distance Red & SWIR (DRS) band
+/**
+ * Adds Distance Red & SWIR (DRS) band to an image.
+ * DRS = sqrt((RED^2) + (SWIR^2))
+ * @param {Object} image - The image to process.
+ * @returns {Object} The image with the DRS band added.
+ */
 exports.addDRS = function(image) {
   var DRS = image.expression(
     'sqrt(((RED) * (RED)) + ((SWIR) * (SWIR)))', {
@@ -34,7 +49,12 @@ exports.addDRS = function(image) {
   return image.addBands([DRS]);
 };
 
-// Add Enhanced Vegetation Index (EVI) band
+/**
+ * Adds Enhanced Vegetation Index (EVI) band to an image.
+ * EVI = 2.5 * ((NIR - RED) / (NIR + 6 * RED - 7.5 * BLUE + 1))
+ * @param {Object} image - The image to process.
+ * @returns {Object} The image with the EVI band added.
+ */
 exports.addEVI = function(image) {
   var EVI = image.expression(
     '2.5 * ((NIR - RED) / (NIR + 6 * RED - 7.5 * BLUE + 1))', {
@@ -45,7 +65,12 @@ exports.addEVI = function(image) {
   return image.addBands([EVI]);
 };
 
-// Add Leaf Area Index (LAI) band
+/**
+ * Adds Leaf Area Index (LAI) band to an image.
+ * LAI = 3.618 * EVI - 0.118
+ * @param {Object} image - The image to process.
+ * @returns {Object} The image with the LAI band added.
+ */
 exports.addLAI = function(image) {
   var LAI = image.expression(
     '3.618 * (EVI) - 0.118', {
@@ -59,7 +84,12 @@ exports.addLAI = function(image) {
   return image.addBands([LAI]);
 };
 
-// Add Normalized Difference Moisture Index (NDMI) band
+/**
+ * Adds Normalized Difference Moisture Index (NDMI) band to an image.
+ * NDMI = (NIR - SWIR) / (NIR + SWIR)
+ * @param {Object} image - The image to process.
+ * @returns {Object} The image with the NDMI band added.
+ */
 exports.addNDMI = function(image) {
   var NDMI = image.expression(
     '(NIR - SWIR) / (NIR + SWIR)', {
@@ -69,19 +99,34 @@ exports.addNDMI = function(image) {
   return image.addBands([NDMI]);
 };
 
-// Add Normalized Difference Snow Index (NDSI) band
+/**
+ * Adds Normalized Difference Snow Index (NDSI) band to an image.
+ * NDSI = (Green - SWIR) / (Green + SWIR)
+ * @param {Object} image - The image to process.
+ * @returns {Object} The image with the NDSI band added.
+ */
 exports.addNDSI = function(image) {
   var NDSI = image.normalizedDifference(['SR_B2', 'SR_B5']).rename('NDSI');
   return image.addBands([NDSI]);
 };
 
-// Add Normalized Difference Vegetation Index (NDVI) band
+/**
+ * Adds Normalized Difference Vegetation Index (NDVI) band to an image.
+ * NDVI = (NIR - Red) / (NIR + Red)
+ * @param {Object} image - The image to process.
+ * @returns {Object} The image with the NDVI band added.
+ */
 exports.addNDVI = function(image) {
   var NDVI = image.normalizedDifference(['SR_B4', 'SR_B3']).rename('NDVI');
   return image.addBands([NDVI]);
 };
 
-// Add Normalized Distance Red & SWIR (NDRS) band
+/**
+ * Adds Normalized Distance Red & SWIR (NDRS) band to an image.
+ * NDRS = (DRS - DRSmin) / (DRSmax - DRSmin)
+ * @param {Object} image - The image to process.
+ * @returns {Object} The image with the NDRS band added.
+ */
 exports.addNDRS = function(image) {
   var NDRS = image.expression(
     '(DRS - DRSmin) / (DRSmax - DRSmin)', {
@@ -92,7 +137,12 @@ exports.addNDRS = function(image) {
   return image.addBands([NDRS]);
 };
 
-// Add Soil Adjusted Vegetation Index (SAVI) band
+/**
+ * Adds Soil Adjusted Vegetation Index (SAVI) band to an image.
+ * SAVI = ((NIR - Red) / (NIR + Red + 0.428)) * 1.428
+ * @param {Object} image - The image to process.
+ * @returns {Object} The image with the SAVI band added.
+ */
 exports.addSAVI = function(image) {
   var SAVI = image.expression(
     '((NIR - R) / (NIR + R + 0.428)) * (1.428)', {
@@ -102,7 +152,12 @@ exports.addSAVI = function(image) {
   return image.addBands([SAVI]);
 };
 
-// Add Shadow Index (SI) band
+/**
+ * Adds Shadow Index (SI) band to an image.
+ * SI = (1 - Blue) * (1 - Green) * (1 - Red)
+ * @param {Object} image - The image to process.
+ * @returns {Object} The image with the SI band added.
+ */
 exports.addSI = function(image) {
   var SI = image.expression(
     '(1 - blue) * (1 - green) * (1 - red)', {
@@ -115,7 +170,11 @@ exports.addSI = function(image) {
 
 // Define Landsat masks functions
 
-// Mask clouds and snow from Landsat images
+/**
+ * Masks clouds and snow from Landsat images.
+ * @param {Object} image - The image to process.
+ * @returns {Object} The image with clouds and snow masked.
+ */
 exports.mask_cloud_snow = function(image) {
   var qa = image.select('QA_PIXEL');
   var cloudsBitMask = 1 << 3; // Cloud mask
@@ -127,7 +186,11 @@ exports.mask_cloud_snow = function(image) {
   return image.updateMask(mask);
 };
 
-// Mask clouds from Landsat images
+/**
+ * Masks clouds from Landsat images.
+ * @param {Object} image - The image to process.
+ * @returns {Object} The image with clouds masked.
+ */
 exports.mask_cloud = function(image) {
   var qa = image.select('QA_PIXEL');
   var cloudsBitMask = 1 << 3; // Cloud mask
@@ -137,17 +200,49 @@ exports.mask_cloud = function(image) {
   return image.updateMask(mask);
 };
 
-// Add snow band based on NDSI values
+/**
+ * Adds a snow band based on NDSI values to an image.
+ * Snow band is true where NDSI > 0.4.
+ * @param {Object} image - The image to process.
+ * @returns {Object} The image with the snow band added.
+ */
 exports.addSnow = function(image) {
   var snow = image.normalizedDifference(['SR_B2', 'SR_B5']).gt(0.4).rename('snow');
   return image.addBands([snow]);
 };
 
-// Create a binary mask for stressed and not stressed forest pixels
-exports.createBinaryMask = function(image) {
-  // Placeholder for complex processing not included in this snippet
-  // This is a simplified version
-  var binaryMask = image.select('NDRS').gt(0.5).rename('NDRS_stressed');
-  return image.addBands([binaryMask]);
-};
 
+
+// Create a binary mask for identifying stressed forest pixels
+// based on the NDRS index.
+
+// Import required masks module
+var masks = require("users/bgcasey/functions:masks");
+
+// Define the band name and threshold for stressed pixel classification
+var bandName = 'NDRS';
+var threshold = 0.5;
+
+/**
+ * Creates a binary mask to identify stressed forest pixels.
+ * Masks out non-forest pixels, applies a threshold to the NDRS band.
+ * Pixels above the threshold are considered stressed.
+ * 
+ * @param {Object} image - The image to process.
+ * @returns {Object} Image with an added binary mask band ('NDRS_stressed').
+ */
+exports.createBinaryMask = function(image) {
+  // Mask non-forest pixels, replace with zero for continuous raster
+  var maskedImage = masks.maskByLandcover(image).unmask(0);
+
+  // Select the NDRS band from the masked image
+  var band = maskedImage.select(bandName);
+  
+  // Apply threshold to identify stressed pixels
+  var binaryMask = band.gt(threshold).rename('NDRS_stressed');
+  
+  // Add binary mask as a new band to the original image
+  var imageWithMask = image.addBands(binaryMask);
+
+  return imageWithMask;
+};
